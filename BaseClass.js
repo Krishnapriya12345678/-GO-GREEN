45bf9abc6894094c7b54ac77d2b9b1824626f232
Krishnapriya12345678 <72981926+Krishnapriya12345678@users.noreply.github.com>
2020-11-17 18:13:21 +0530

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,22 @@
+class BaseClass{
+    constructor(x, y,radius) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, this.radius, options);
+        this.radius=this.body.radius
+        this.image = loadImage("Waste.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+      }
+}
---
+++ waste.js
@@ -0,0 +1,12 @@
+class Waste extends BaseClass{
+    constructor(x,y){
+        super(x,y,70)
+        this.image=loadImage('Waste.png')
+    }
+
+display(){
+    super.display();
+}
+
+
+}
